fdc7036b0a296389382df36ee7995fec2fc3ce90
nabinbogati <nabin31bogati@gmail.com>
2025-09-11 19:58:20 +0545

    build: homepage & base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: This should match your repository name
-  base: '/portfolio', 
-})
+  base: "/my-portfolio",
+});
